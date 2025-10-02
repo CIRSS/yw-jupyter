0a4742a4f3b0f58d458291a9df38fd8e74137899
Shin-Rong Tsai <turquoisea.tsai@gmail.com>
2025-10-02 13:16:53 -0500

    Call yw-core and get feedback through directly reading the stdout.

--- src/yw-core.ts
+++ src/yw-core.ts
@@ -1,6 +1,6 @@
 import { Kernel, KernelMessage } from '@jupyterlab/services';
 import { Notification } from '@jupyterlab/apputils';
-import { IOutput } from '@jupyterlab/nbformat';
+import { IStream } from '@jupyterlab/nbformat';
 
 export interface YWEdge {
   id: string;
@@ -8,9 +8,13 @@ export interface YWEdge {
   target: string;
 }
 
-// TODO: compute the edges using yw-core
 /**
- * Compute the edges for the YesWorkflow visualization.
+ * Main API for compute the edges for the YesWorkflow visualization.
+ * Must have a kernel with yw-core installed.
+ * @todo need to create a temporary storage for cells data, so that we don't overwrite existing Python objects
+ * @param kernel kernel connection to call yw-core
+ * @param input_cells list of code string in each cell
+ * @returns YWEdge[] computed by yw-core
  */
 export function computeEdges(
   kernel: Kernel.IKernelConnection | undefined | null,
@@ -27,20 +31,32 @@ export function computeEdges(
 
   // call yw-core to compute edges silently
   const test_code =
-    "import ast, sys\nast.parse('a=1')\nprint('hello world')" +
-    '\nprint(sys.version)\n';
+    "import ast, sys\nast.parse('a=1')\na=1\n" +
+    '\nprint(sys.version)\na';
   const exec_result = kernel.requestExecute({
     code: test_code,
     silent: false,
     store_history: false
   });
   console.log('[computeEdges] ', exec_result);
+  let output: string | string[] | null | undefined = null;
   exec_result.onIOPub = (msg: KernelMessage.IIOPubMessage) => {
-    if (msg.header.msg_type === 'stream') {
-      const content = msg.content as IOutput;
-      console.log('[computeEdges] stream: ', content);
+    if (msg.header.msg_type === 'stream') { // TODO: how to make yw-core show the output? (https://jupyterlab.readthedocs.io/en/3.4.x/api/modules/nbformat.html#multilinestring)
+      const content = msg.content as IStream;
+      output = content.text;
+      console.log('[computeEdges] stream output: ', output);
     }
   };
 
+  // TODO: clear the Python state
+
+  return parseYWCoreOutput(output);
+}
+
+/**
+ * // TODO: compute the edges using yw-core
+ * @param output
+ */
+function parseYWCoreOutput(output: string | string[] | null | undefined): YWEdge[] {
   return [{ id: 'e0-1', source: '0', target: '1' }];
 }
