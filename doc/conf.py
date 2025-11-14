5f8d5c92155fbb8922958e6a298fe2a5a09de2c2
Shin-Rong Tsai <turquoisea.tsai@gmail.com>
2025-11-14 14:52:01 -0600

    Initialize doc.

---
+++ doc/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'jupyter-yesworkflow'
+copyright = '2025, CIRSS'
+author = 'CIRSS'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'shibuya'
+html_theme_options = {
+    "page_layout": "default"
+}
+html_static_path = ['_static']
